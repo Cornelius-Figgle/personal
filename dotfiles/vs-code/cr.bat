b367c4d93f5d697d92c0573a2376dcb5e627f06d
Cornelius-Figgle <max@fullimage.net>
2022-12-28 12:18:32 +0000

    added vsc files

---
+++ dotfiles/vs-code/cr.bat
@@ -0,0 +1,9 @@
+mklink "C:\Users\Max.Harrison\AppData\Roaming\Code\Preferences" "T:\personal\dotfiles\vs-code\Preferences"
+
+mklink "C:\Users\Max.Harrison\AppData\Roaming\Code\languagepacks.json" "T:\personal\dotfiles\vs-code\languagepacks.json"
+
+mklink "C:\Users\Max.Harrison\AppData\Roaming\Code\User\settings.json" "T:\personal\dotfiles\vs-code\User\settings.json"
+
+mklink "C:\Users\Max.Harrison\AppData\Roaming\Code\User\tasks.json" "T:\personal\dotfiles\vs-code\User\tasks.json"
+
+mklink "C:\Users\Max.Harrison\AppData\Roaming\Code\User\keybindings.json" "T:\personal\dotfiles\vs-code\User\keybindings.json"
