65cebaa0adc553d8b42555a8523646f19289b717
Cornelius-Figgle <max@fullimage.net>
2023-04-10 12:34:24 +0100

    magic, part II

--- Rainmeter/Skins/EqClock/@Resources/LuaScripts/script.lua
+++ Rainmeter/Skins/EqClock/@Resources/LuaScripts/script.lua
@@ -11,33 +11,35 @@ function Initialize()
 	yOffset = SKIN:GetH() / 2
 
 	print('`xOffset` is: ' .. xOffset .. ' and `yOffset` is: ' .. yOffset)
-
-	--[[
-		os = require('os')
-		math = require('math')
-	]]
 end
 
-function calcDigitalPosition(whichVar)	
-	-- print('`calcDigitalPosition` has been called with value `' .. whichVar .. '`')
-	
+function calcDigitalPosition(whichVar)		
+	-- gets the time, simple
 	local hours = os.date('%I')  -- 12hr format is %I
 	local minutes = os.date('%M')
 
+	-- finds angle to the 3 lines, relative to the North line (ie the y-axis)
 	local angleOfHourLine = hours / 12 * 360
 	local angleOfMinuteLine = minutes / 60 * 360
-	local sumOfAngles = angleOfHourLine + angleOfMinuteLine
-	local angleOfBisectionLine = sumOfAngles > 180 and (360 - sumOfAngles) / 2 or sumOfAngles / 2  -- if reflexive, find the opposite angle
-	local angleOfNormalToBisectionLine = angleOfBisectionLine + 180  -- inverts the angle (again)
+	local angleOfBisectionLine = math.abs(angleOfHourLine - angleOfMinuteLine) / 2 + math.min(angleOfHourLine, angleOfMinuteLine)
 
+	-- magic
+	-- info: https://math.stackexchange.com/questions/260096/find-the-coordinates-of-a-point-on-a-circle
 	local radiusOfCircle = 70  -- random value to make the text fit
-	local xPoint = radiusOfCircle * math.sin(math.rad(angleOfNormalToBisectionLine))
-	local yPoint = radiusOfCircle * math.cos(math.rad(angleOfNormalToBisectionLine))
+	local xPoint = radiusOfCircle * math.sin(math.rad(angleOfBisectionLine))
+	local yPoint = radiusOfCircle * math.cos(math.rad(angleOfBisectionLine))
 
+	-- rounding because integers are better
 	local xPointWithOffset = math.floor(xPoint + xOffset)
 	local yPointWithOffset = math.floor(yPoint + yOffset)
 
+	-- multiply by -1 to rotate by 180
+	-- matrices would be more elegant and prob better but I cba
+	local rotatedXPointWithOffset = math.floor(xPoint + xOffset)
+	local rotatedYPointWithOffset = math.floor(yPoint + yOffset)
+
 	--[[
+		-- debugging local vars
 		local i = 1
 		repeat
 			local k, v = debug.getlocal(1, i)
@@ -48,10 +50,13 @@ function calcDigitalPosition(whichVar)
 		until k == nil
 	]]
 
+	-- rainmeter only accepts one return value
+	-- so we run the script twice and return a diff value
+	-- info: https://forum.rainmeter.net/viewtopic.php?t=38703
 	if string.lower(whichVar) == 'x' then
-		return xPointWithOffset
+		return rotatedXPointWithOffset
 	elseif string.lower(whichVar) == 'y' then
-		return yPointWithOffset
+		return rotatedYPointWithOffset
 	else
 		print('You need to define which variable you want!')
 		return 0
