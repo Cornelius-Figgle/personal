4f82ac3efe74286ffda04578e5a584b8bfa44bfe
Cornelius-Figgle <max@fullimage.net>
2023-05-01 18:08:33 +0100

    no

--- dotfiles/vs-code/cr.bat
+++ dotfiles/vs-code/cr.bat
@@ -1,9 +1,9 @@
-mklink "C:\Users\Max.Harrison\AppData\Roaming\Code\Preferences" "T:\personal\dotfiles\vs-code\Preferences"
+mklink "C:\Users\Max.Harrison\AppData\Roaming\Code\Preferences" "C:\Users\Max.Harrison\source\personal\dotfiles\vs-code\Preferences"
 
-mklink "C:\Users\Max.Harrison\AppData\Roaming\Code\languagepacks.json" "T:\personal\dotfiles\vs-code\languagepacks.json"
+mklink "C:\Users\Max.Harrison\AppData\Roaming\Code\languagepacks.json" "C:\Users\Max.Harrison\source\personal\dotfiles\vs-code\languagepacks.json"
 
-mklink "C:\Users\Max.Harrison\AppData\Roaming\Code\User\settings.json" "T:\personal\dotfiles\vs-code\User\settings.json"
+mklink "C:\Users\Max.Harrison\AppData\Roaming\Code\User\settings.json" "C:\Users\Max.Harrison\source\personal\dotfiles\vs-code\User\settings.json"
 
-mklink "C:\Users\Max.Harrison\AppData\Roaming\Code\User\tasks.json" "T:\personal\dotfiles\vs-code\User\tasks.json"
+mklink "C:\Users\Max.Harrison\AppData\Roaming\Code\User\tasks.json" "C:\Users\Max.Harrison\source\personal\dotfiles\vs-code\User\tasks.json"
 
-mklink "C:\Users\Max.Harrison\AppData\Roaming\Code\User\keybindings.json" "T:\personal\dotfiles\vs-code\User\keybindings.json"
+mklink "C:\Users\Max.Harrison\AppData\Roaming\Code\User\keybindings.json" "C:\Users\Max.Harrison\source\personal\dotfiles\vs-code\User\keybindings.json"
