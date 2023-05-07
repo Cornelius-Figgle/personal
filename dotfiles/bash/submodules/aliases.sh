7940cbc4e12aa5b8515caa2da3429841f226cd35
Cornelius-Figgle <max@fullimage.net>
2023-05-07 09:39:38 +0100

    GL:ANCES

--- dotfiles/bash/submodules/aliases.sh
+++ dotfiles/bash/submodules/aliases.sh
@@ -11,3 +11,5 @@ if [ $HOSTNAME = mimic3 ]; then
     alias ter='/mnt/p/00\ Max/mimic/ter_start.sh'
     alias mc='/mnt/p/00\ Max/mimic/mc_start.sh'
 fi
+
+alias glances='glances --percpu --disable-irix --separator --programs'
