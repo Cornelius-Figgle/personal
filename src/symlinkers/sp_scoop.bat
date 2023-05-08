f806c3763f7cb40a228d43cd4e0dac7259d23199
Cornelius-Figgle <max@fullimage.net>
2023-05-08 18:28:41 +0100

    SYM LINK BAT

--- src/symlinkers/sp_scoop.bat
+++ src/symlinkers/sp_scoop.bat
@@ -1,7 +1,7 @@
-mklink "C:\users\max.harrison\appdata\roaming\spicetify\config-xpui.ini"
-mklink /D "C:\Users\Max.Harrison\scoop\apps\spicetify-cli\current\Extensions"
-mklink /D "C:\Users\Max.Harrison\scoop\apps\spicetify-cli\current\Themes"
-mklink "C:\Users\Max.Harrison\scoop\apps\spicetify-cli\current\css-map.json"
+del "C:\users\max.harrison\appdata\roaming\spicetify\config-xpui.ini"
+rd "C:\Users\Max.Harrison\scoop\apps\spicetify-cli\current\Extensions"
+rd "C:\Users\Max.Harrison\scoop\apps\spicetify-cli\current\Themes"
+del "C:\Users\Max.Harrison\scoop\apps\spicetify-cli\current\css-map.json"
 
 mklink "C:\users\max.harrison\appdata\roaming\spicetify\config-xpui.ini" "C:\users\max.harrison\source\personal\dotfiles\spicetify\config-xpui.ini"
 mklink /D "C:\Users\Max.Harrison\scoop\apps\spicetify-cli\current\Extensions" "c:\users\max.harrison\source\personal\dotfiles\spicetify\Extensions"
