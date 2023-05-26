ecb42708a014ee6b43c9438a96c6d6b9801744b9
Cornelius-Figgle <max@fullimage.net>
2023-05-26 17:12:31 +0100

    ues

--- src/symlinkers/git_scoop.bat
+++ src/symlinkers/git_scoop.bat
@@ -1,5 +1,5 @@
 del "C:\Users\Max.Harrison\.gitconfig"
 del "C:\Users\Max.Harrison\scoop\apps\git\current\etc\bash.bashrc"
 
-mklink "C:\Users\Max.Harrison\.gitconfig" "t:\source\personal\dotfiles\git\.gitconfig"
+mklink "c:\users\max.harrison\.gitconfig" "t:\source\personal\dotfiles\git\.win.gitconfig"
 mklink "C:\Users\Max.Harrison\scoop\apps\git\current\etc\bash.bashrc" "t:\source\personal\dotfiles\bash\.bashrc"
