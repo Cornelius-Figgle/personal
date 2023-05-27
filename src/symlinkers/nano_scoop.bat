94fc4378b79069957cb55d3d5e733de9e41e5184
Cornelius-Figgle <max@fullimage.net>
2023-05-27 09:06:01 +0100

    symlinks

---
+++ src/symlinkers/nano_scoop.bat
@@ -0,0 +1,2 @@
+del "C:\users\max.harrison\.nanorc"
+mklink "C:\users\max.harrison\.nanorc" "C:\Users\Max.Harrison\source\personal\dotfiles\nano\.win.nanorc"
--- src/symlinkers/sp_scoop.bat
+++ src/symlinkers/sp_scoop.bat
@@ -5,7 +5,7 @@ rd /s /q "C:\Users\Max.Harrison\scoop\apps\spicetify-cli\current\Extensions"
 rd /s /q "C:\Users\Max.Harrison\scoop\apps\spicetify-cli\current\Themes"
 del /q "C:\Users\Max.Harrison\scoop\apps\spicetify-cli\current\css-map.json"
 
-mklink "C:\users\max.harrison\appdata\roaming\spicetify\config-xpui.ini" "C:\Users\Max.Harrison\source\personal\dotfiles\spicetify\config-xpui.ini"
+mklink "C:\users\max.harrison\appdata\roaming\spicetify\config-xpui.ini" "C:\Users\Max.Harrison\source\personal\dotfiles\spicetify\win.config-xpui.ini"
 mklink /D "C:\Users\Max.Harrison\scoop\apps\spicetify-cli\current\Extensions" "C:\Users\Max.Harrison\source\personal\dotfiles\spicetify\Extensions"
 mklink /D "C:\Users\Max.Harrison\scoop\apps\spicetify-cli\current\Themes" "C:\Users\Max.Harrison\source\personal\dotfiles\spicetify\Themes" 
 mklink "C:\Users\Max.Harrison\scoop\apps\spicetify-cli\current\css-map.json" "C:\Users\Max.Harrison\source\personal\dotfiles\spicetify\css-map.json"
