a751b8ebd6a7b3de3c4922d4b9225c93665eb011
Cornelius-Figgle <max@fullimage.net>
2023-05-31 10:22:23 +0100

    poe

--- src/symlinkers/poetry_scoop.bat
+++ src/symlinkers/poetry_scoop.bat
@@ -1,1 +1,2 @@
+del C:\users\max.harrison\appdata\roaming\pypoetry\config.toml
 mklink C:\Users\Max.Harrison\AppData\Roaming\pypoetry\config.toml C:\Users\Max.Harrison\source\personal\dotfiles\pypoetry\config.toml
