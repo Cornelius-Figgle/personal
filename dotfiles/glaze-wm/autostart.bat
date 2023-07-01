6e963884d2564c3fbcb947b649054701b29e7687
Cornelius-Figgle <max@fullimage.net>
2023-07-01 22:03:01 +0100

    GLAZE EXPLOER??

---
+++ dotfiles/glaze-wm/autostart.bat
@@ -0,0 +1,3 @@
+@echo off
+taskkill /IM explorer.exe /F
+glazewm.exe
--- src/symlinkers/glaze.bat
+++ src/symlinkers/glaze.bat
@@ -1,2 +1,5 @@
 rmdir c:\users\max.harrison\.glaze-wm
 mklink /D c:\users\max.harrison\.glaze-wm c:\users\max.harrison\source\personal\dotfiles\glaze-wm
+
+del "C:\Users\Max.Harrison\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\autostart.bat"
+mklink "C:\Users\Max.Harrison\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\autostart.bat" "c:\users\max.harrison\source\personal\dotfiles\glaze-wm\autostart.bat"
