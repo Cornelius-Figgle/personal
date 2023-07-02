e7a29af16ff9a033e8e2fe36717c04bb17438a8f
Cornelius-Figgle <max@fullimage.net>
2023-07-02 14:34:40 +0100

    no explorer

--- dotfiles/glaze-wm/autostart.bat
+++ dotfiles/glaze-wm/autostart.bat
@@ -2,7 +2,7 @@
 
 START /B "" glazewm.exe
 
-START /B "" taskkill /IM explorer.exe /F
+:: START /B "" taskkill /IM explorer.exe /F
 
 if %COMPUTERNAME%==NIGHTCRAWLER START /B "" "C:\Users\Max.Harrison\source\personal\ahk\base_remaps\ibm_remaps.ahk"
 if %COMPUTERNAME%==NIGHTCRAWLER START /B "" "C:\Users\Max.Harrison\source\personal\ahk\tools\PANIK.ahk"
