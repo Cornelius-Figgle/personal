542ad348b8233bfd7dbdb9aad1feb6cd89137c26
Cornelius-Figgle <max@fullimage.net>
2023-07-02 13:46:41 +0100

    cmd

--- dotfiles/glaze-wm/autostart.bat
+++ dotfiles/glaze-wm/autostart.bat
@@ -6,3 +6,5 @@ START /B "" taskkill /IM explorer.exe /F
 
 if %COMPUTERNAME%==NIGHTCRAWLER START /B "" "C:\Users\Max.Harrison\source\personal\ahk\base_remaps\ibm_remaps.ahk"
 if %COMPUTERNAME%==NIGHTCRAWLER START /B "" "C:\Users\Max.Harrison\source\personal\ahk\tools\PANIK.ahk"
+
+:: make sure to `auto hide the taskbar` as well otherwise Glaze won't recognise the gap!
